9df7fa52a870886d696f5fbac8d836b0123feeb3
jayho <sox3579@gmail.com>
2022-05-23 01:18:25 +0900

    feat : implement wishlist_show_save

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 from django.contrib.auth import get_user_model
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, WishListSerializer
+from .models import WishList
+
 
 User = get_user_model()
 
@@ -15,3 +17,42 @@ def profile(request, username):
     user = get_object_or_404(User, username=username)
     serializer = ProfileSerializer(user)
     return Response(serializer.data)
+
+
+@api_view(['GET','POST'])
+def wishlist_show_save(request):
+    
+    #show
+    if request.method == 'GET':
+        user = request.user
+        wish_list = get_list_or_404(WishList,pk=user.pk)
+        serializers = WishListSerializer(wish_list, many=True)
+        return Response(serializers.data)
+
+    # save
+    elif request.method == 'POST':
+        
+        user = request.user
+        movie_id = request.data['movie_id']
+
+
+        wish_list = WishList.objects.all().filter(user_id=user)
+
+        # 위시무비 id 값만 불러오기
+        wish_movie_id = []
+        for wish in wish_list:
+            wish_movie_id.append(wish.movie_id)
+
+        # wish_movie_id값안에 값이 있으면 ==> 삭제
+        if movie_id in wish_movie_id:
+            wish_object = get_object_or_404(WishList,user_id=user,movie_id=movie_id)
+            wish_object.delete()
+            serializers = WishListSerializer(wish_object)
+            return Response(serializers.data)
+
+        # 없으면 ==> 추가
+        else:
+            serializers = WishListSerializer(data=request.data)
+            if serializers.is_valid(raise_exception=True):
+                serializers.save(user=user)
+                return Response(serializers.data)
