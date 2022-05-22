a88994ff77350d8123cb156b1ebfc761470bf3e0
josajang98 <na0173@naver.com>
2022-05-23 02:06:34 +0900

    refactor : ArticlesView method 분리
    axios 요청을 해서 데이터를 가져오는 method와 가져온 데이터에서 랜덤한 movie 데이터를 뽑는 method를 분리함

--- final-pjt-front/src/views/ArticlesView.vue
+++ final-pjt-front/src/views/ArticlesView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <MainMovieCard :backdrop-path="backdropPath" :title="title"></MainMovieCard>
+    {{getRandMovieData()}}
+    <MainMovieCard :backdrop-path="mainMovieBackdropPath" :title="mainMovietitle"></MainMovieCard>
   </div>
 </template>
 
@@ -9,46 +10,69 @@ import MainMovieCard from '@/components/MainMovieCard.vue'
 import axios from 'axios'
 import drf from '@/api/drf'
 import _ from 'lodash'
+
+const imgUrl='https://image.tmdb.org/t/p/w500/'
 export default {
   
   name: 'ArticlesView',
 
   data() {
     return {
-      backdropPath:'',
-      title:'',
+      nowPlayingMovieList:'',
+      mainMovieBackdropPath:'',
+      mainMovietitle:'',
+
     };
   },
   created(){
-    this.getMainMovie()
+    this.getNowPlayingMovieList()
+    
   },
   components:{
     MainMovieCard,
   },
   mounted() {
-    
+    this.getRandMovieData()
   },
 
   methods: {
-    getMainMovie(){
+    getNowPlayingMovieList(){
       axios({
         url: drf.tmdb.nowPlaying(),
         method: 'get',
       })
         .then(res=> {
-          
-          const randIdx=_.random(0,19)
-          
-          this.backdropPath='https://image.tmdb.org/t/p/w500/'+res.data.results[randIdx].backdrop_path
-          this.title=res.data.results[randIdx].title
-          
+          this.nowPlayingMovieList=res.data.results
+
         })
-        // .catch(err => {
-        //   console.error(err.response.data)
-        //   commit('SET_AUTH_ERROR', err.response.data)
-        // })
+        .catch(err => {
+          console.error(err.response.data)
+        })
+    },
+    getGenreMovie(){
+      axios({
+        url: drf.tmdb.nowPlaying(),
+        method: 'get',
+      })
+        .then(res=> {
+          res
+        })
+        .catch(err => {
+          console.error(err.response.data)
+        })
+    },
+    getRandMovieData(){
+      const randNum=_.random(0,19)
+
+      const randMovie=this.nowPlayingMovieList[randNum]
+      console.log(randMovie)
+      const backdropPath=imgUrl+randMovie.backdrop_path
+      const title=randMovie.title
+      this.mainMovieBackdropPath=backdropPath
+      this.mainMovietitle=title
     }
   },
+
 };
 </script>
 
