2cdbe1cfe4c78224d64bd5a9a27dcc518aa136d1
josajang98 <na0173@naver.com>
2022-05-22 21:52:45 +0900

    refactor : MainMovieCard 데이터 흐름 리팩토링
    MainMovieCard에서 axios요청을 보내서 데이터를 가져오던걸
    부모 컴포넌트인 ArticlesView에서 데이터를 가져와서 props로 전달

--- final-pjt-front/src/views/ArticlesView.vue
+++ final-pjt-front/src/views/ArticlesView.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
-    <MainMovieCard></MainMovieCard>
+    <MainMovieCard :backdrop-path="backdropPath" :title="title"></MainMovieCard>
   </div>
 </template>
 
 <script>
 import MainMovieCard from '@/components/MainMovieCard.vue'
-
+import axios from 'axios'
+import drf from '@/api/drf'
+import _ from 'lodash'
 export default {
+  
   name: 'ArticlesView',
 
   data() {
     return {
-      
+      backdropPath:'',
+      title:'',
     };
+  },
+  created(){
+    this.getMainMovie()
   },
   components:{
     MainMovieCard,
@@ -23,7 +30,24 @@ export default {
   },
 
   methods: {
-    
+    getMainMovie(){
+      axios({
+        url: drf.tmdb.nowPlaying(),
+        method: 'get',
+      })
+        .then(res=> {
+          
+          const randIdx=_.random(0,19)
+          
+          this.backdropPath='https://image.tmdb.org/t/p/w500/'+res.data.results[randIdx].backdrop_path
+          this.title=res.data.results[randIdx].title
+          
+        })
+        // .catch(err => {
+        //   console.error(err.response.data)
+        //   commit('SET_AUTH_ERROR', err.response.data)
+        // })
+    }
   },
 };
 </script>
