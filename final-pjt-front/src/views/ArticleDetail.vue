894fb25199fa76ec3ba459fd61aa71b6b642b63b
josajang98 <na0173@naver.com>
2022-05-24 00:46:58 +0900

    feat : 디테일 페이지에  리뷰리스트 추가

--- final-pjt-front/src/views/ArticleDetail.vue
+++ final-pjt-front/src/views/ArticleDetail.vue
@@ -5,40 +5,56 @@
     <p>{{releaseDate}}</p>
     <p>{{voteAverage}}</p>
     <img :src="posterPath" alt="">
-
+    <!-- <img src="../assets/18.png" alt="" style="width: 600px;height: 900px;"> -->
     <a v-if="mainTrailerUrl" :href="mainTrailerUrl">미리보기</a>
+
+    <review-card 
+    v-for="review in reviewList" 
+    :review="review"
+    :key="review.id"></review-card>
   </div>
 </template>
 
 <script>
+import ReviewCard from '../components/ReviewCard.vue'
 import axios from 'axios';
 import drf from '@/api/drf'
+import {mapGetters} from 'vuex'
 
 const imgUrl='https://image.tmdb.org/t/p/w500/'
 
 export default {
+  components: { ReviewCard },
   name: 'ArticleDetail',
 
   data() {
     return {
-      movie_id:this.$route.params.movie_id,
+      // 영화 데이터
+      movieId:this.$route.params.movie_id,
       posterPath:'',
       title:'',
       overview:'',
       releaseDate:'',
       voteAverage:'',
       mainTrailerUrl:'',
+
+
+      // 리뷰 데이터
+      reviewList:'',
     };
   },
 
   created() {
     this.getMovieData(),
-    this.getMovieVideo()
+    this.getMovieVideo(),
+    this.getReviewList()
   },
-
+  computed:{
+    ...mapGetters(['authHeader'])
+  },
   methods: {
     async getMovieData(){
-      const response=await axios.get(drf.tmdb.detail(this.movie_id))
+      const response=await axios.get(drf.tmdb.detail(this.movieId))
 
       this.posterPath=imgUrl+response.data.poster_path
       this.title=response.data.title
@@ -47,11 +63,20 @@ export default {
       this.voteAverage=response.data.vote_average
     },
     async getMovieVideo(){
-      const response=await axios.get(drf.tmdb.videos(this.movie_id))
+      const response=await axios.get(drf.tmdb.videos(this.movieId))
       const youtubeUrl="https://www.youtube.com/watch?v="
       
       this.mainTrailerUrl=youtubeUrl+response.data.results[0].key
-      
+    },
+
+    async getReviewList(){
+      const response=await axios({
+        url: drf.articles.reviewList(this.movieId),
+        method: 'get',
+        headers: this.authHeader,
+      })
+      this.reviewList=response.data.serializer_data
+      console.log(this.reviewList)
     }
   },
 };
