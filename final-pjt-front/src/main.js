fd0c7e55bf99624219a25ecc53e8e0b6ea98f3f3
josajang98 <na0173@naver.com>
2022-05-24 17:38:01 +0900

    chore :merge

--- final-pjt-front/src/main.js
+++ final-pjt-front/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
+
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
