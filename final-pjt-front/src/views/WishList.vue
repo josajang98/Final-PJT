84987234875f8704cbe0967b1f2881ce503f4613
josajang98 <na0173@naver.com>
2022-05-24 14:18:16 +0900

    feat : wishList 생성

---
+++ final-pjt-front/src/views/WishList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <movie-card
+    v-for="movie in wishList"
+      :movie-id="movie.movie_id"
+      :movie-poster-path="movie.poster_path"
+      :key="movie.id"
+      ></movie-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import drf from '@/api/drf'
+import {mapGetters} from 'vuex'
+import MovieCard from '../components/MovieCard.vue';
+export default {
+  name: 'WishList',
+
+  data() {
+    return {
+      wishList:'',
+    };
+  },
+  components: { MovieCard },
+  computed:{
+    ...mapGetters(['authHeader'])
+  },
+  created(){
+    this.getWishList()
+  },
+  methods: {
+    getWishList(){
+      axios({
+        url: drf.accounts.wishList(),
+        method: 'get',
+        headers: this.authHeader,
+      }).then((res)=>{
+        this.wishList=res.data
+        console.log(this.wishList)
+      })
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
