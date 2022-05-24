38f516ddd0d10feec437a52531f5039fd2e26810
josajang98 <na0173@naver.com>
2022-05-24 17:16:08 +0900

    feat : 프로필페이지 내댓글 보여주기  기능추가

--- final-pjt-front/src/views/ProfileView.vue
+++ final-pjt-front/src/views/ProfileView.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <p></p>
+    <p>내가 쓴 리뷰</p>
+    <review-card 
+    v-for="review in userWriteReviewList" 
+    :review="review"
+    :key="review.id"
+    @getReview="getProfileData"
+    ></review-card>
+    <br>
+    <p>내가 좋아요 단 리뷰</p>
+    <review-card 
+    v-for="review in userLikeReviewList" 
+    :review="review"
+    :key="review.created_at"
+    @getReview="getProfileData"
+    ></review-card>
+    
   </div>
 </template>
 
@@ -8,17 +23,26 @@
 import {mapGetters} from 'vuex'
 import axios from 'axios'
 import drf from '@/api/drf'
+import ReviewCard from '../components/ReviewCard.vue'
 export default {
   name: 'ProfileView',
 
   data() {
     return {
-      userName:this.$route.params.username
+      userName:this.$route.params.username,
+      userWriteReviewList:'',
+      userLikeReviewList:'',
     };
+  },
+  components:{
+    ReviewCard
   },
 
   computed: {
     ...mapGetters(['authHeader'])
+  },
+  created(){
+    this.getProfileData()
   },
 
   methods: {
@@ -28,12 +52,13 @@ export default {
         method: 'get',
         headers: this.authHeader,
       })
-      console.log(response)
-    }
+      this.userWriteReviewList=response.data.review_set
+      this.userLikeReviewList=response.data.like_review
+      console.log(this.userWriteReviewList)
+    },
+    
   },
-  created(){
-    this.getProfileData()
-  }
+  
 };
 </script>
 
