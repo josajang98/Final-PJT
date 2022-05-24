0b024b9975768727cf62397a19c22972c8d12abc
ssomm-k <kww0915@gmail.com>
2022-05-24 17:23:43 +0900

    feat : background css

--- final-pjt-front/src/views/MainUnlogin.vue
+++ final-pjt-front/src/views/MainUnlogin.vue
@@ -3,7 +3,7 @@
     <p>심심하신가요?</p>
     <p>장르 월드컵을 통해 당신에게 맞는</p>
     <p>영화를 추천 받으세요!</p>
-    <button @click="login">잘골라줘유 START</button>
+    <button @click="login" class="custom-btn btn-14">잘골라줘유 START</button>
   </div>
 </template>
 
@@ -32,5 +32,59 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+p {
+  font-size: 50px;
+  // text-shadow: 2px 2px 2px gray;
+}
+.custom-btn {
+  font-size: 53px;
+    text-shadow: 2px 2px 2px black;
+  width: 500px;
+  height: 100px;
+  color: #fff;
+  border-radius: 25px;
+  padding: 10px 25px;
+  background: transparent;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: inline-block;
+   box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
+   7px 7px 20px 0px rgba(0,0,0,.1),
+   4px 4px 5px 0px rgba(0,0,0,.1);
+  outline: none;
+}
 
+.btn-14 {
+  background: #FF8307;
+  border: none;
+  z-index: 1;
+}
+.btn-14:after {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 0;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  border-radius: 25px;
+  background-color: #eaf818;
+  background-image: linear-gradient(315deg, #eaf818 0%, #f6fc9c 74%);
+   box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
+   7px 7px 20px 0px rgba(0,0,0,.1),
+   4px 4px 5px 0px rgba(0,0,0,.1);
+  transition: all 0.3s ease;
+}
+.btn-14:hover {
+  color: #000;
+}
+.btn-14:hover:after {
+  top: auto;
+  bottom: 0;
+  height: 100%;
+}
+.btn-14:active {
+  top: 2px;
+}
 </style>
