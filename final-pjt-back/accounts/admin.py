cb1931b0b9305eecbfa430fee0de2defedac7428
jayho <sox3579@gmail.com>
2022-05-24 21:25:36 +0900

    feat : registed admin

--- final-pjt-back/accounts/admin.py
+++ final-pjt-back/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import WishList
 # Register your models here.
+
+class WishListAdmin(admin.ModelAdmin):
+    list_display = ('user','movie_id')
+
+
+admin.site.register(WishList,WishListAdmin)
