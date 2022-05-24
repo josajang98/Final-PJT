9374da4fd88d59a7c02dd39b0225111d77a1bcd6
jayho <sox3579@gmail.com>
2022-05-24 23:52:17 +0900

    chore : modifed little

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,21 +1,40 @@
 <template>
-  <div id="app">
+  <div id="app" class = 'container'>
+
     <nav v-if="!isLoggedIn" class="navbar navbar-expand-lg">
       <router-link to="/" exact-active-class="active"><img src="./assets/logo.png" alt="asd"></router-link> 
     </nav>
-    <nav v-if="isLoggedIn" class="navbar navbar-expand-lg">
+    <nav  v-if="isLoggedIn" class="navbar navbar-expand-lg">
       <router-link exact-active-class="active" :to="{ name: 'articles'}" >
         <img src="./assets/logo.png" alt="asd">
       </router-link> 
-      
-      <router-link :to="{ name: 'genrewc'}">장르 월드컵</router-link> 
-      <router-link :to="{ name: 'wishList'}">내가 찜한 목록</router-link> 
-      <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-      </form> 
-      <router-link :to="{ name: 'profile', params: {username} }">profile</router-link> 
-      <router-link :to="{ name: 'logout' }">Logout</router-link>
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link :to="{ name: 'genrewc'}">장르 월드컵</router-link> 
+            </li>
+          </ul>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <li class="nav-item">
+              <router-link :to="{ name: 'wishList'}">내가 찜한 목록</router-link> 
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'profile', params: {username} }">profile</router-link> | 
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'logout' }">Logout</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
     <router-view/>
   </div>
