14ae83a23ed9b7ec571fce0d843df96a08eef064
ssomm-k <kww0915@gmail.com>
2022-05-26 01:15:19 +0900

    feat : person detail ver.1

--- final-pjt-front/src/views/PersonDetail.vue
+++ final-pjt-front/src/views/PersonDetail.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-    <p>이름 : {{personData[index].name}}</p>
-    <p>성별 : {{personData[index].gender}}</p>
-    <img :src="profilePath" alt="">
-    <div class="container">
-      <div class="row bg-white bg-opacity-10 justify-content-center">
-        <MovieCard
-          v-for="movie in personMovie"
-          :movie-id="movie.id"
-          :movie-poster-path="movie.poster_path"
-          :key="movie.id"
-          class="col-lg-2 col-md-3 col-sm-4"
-        ></MovieCard>
+  <div class="d-flex align-items-center">
+    <div class="flex-shrink-0">
+      <img :src="profilePath" alt="">
+    </div>
+    <div class="flex-grow-1 ms-3">
+      <p>이름 : {{personData[index].name}}</p>
+      <p>성별 : {{personData[index].gender}}</p>
+      <div class="my-custom-scrollbar my-custom-scrollbar-primary">
+        <div class="row bg-white bg-opacity-10 justify-content-center">
+          <MovieCard
+            v-for="movie in personMovie"
+            :movie-id="movie.id"
+            :movie-poster-path="movie.poster_path"
+            :key="movie.id"
+            class="col-lg-2 col-md-3 col-sm-4"
+          ></MovieCard>
+        </div>
       </div>
     </div>
   </div>
@@ -62,5 +66,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+img{
+  width:75%;
+  height: auto;
+}
+.my-custom-scrollbar {
+  position: relative;
+  width: 800px;
+  height: 400px;
+  overflow: auto;
+}
 </style>
