c1024ae6425679a3005a6220dba2327f7ac8e586
jayho <sox3579@gmail.com>
2022-05-26 20:20:04 +0900

    feat : register movie

--- final-pjt-back/movies/admin.py
+++ final-pjt-back/movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from models import Movie
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title')
+
+
+admin.site.register(Movie,MovieAdmin)
