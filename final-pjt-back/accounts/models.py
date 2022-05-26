1dd9ff0363ace0095ccef013015565110c84f0f4
jayho <sox3579@gmail.com>
2022-05-26 23:14:22 +0900

    fix : change limit length in Review model and add title, relead_date,is_wish in Wish model

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -11,6 +11,9 @@ class User(AbstractUser):
 class WishList(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_wish = models.BooleanField(default=True)
+    title = models.CharField(max_length=100)
+    release_date = models.CharField(max_length=50)
     movie_id = models.IntegerField()
     poster_path = models.CharField(max_length=100)
 
